25a5ff18200bb7a3b9d8ad67011890c4e65ac320
rongrlin <allisonlin.r@berkeley.edu>
2023-12-05 16:53:45 -0800

    style changes to dashboard

--- frontend/src/signup/Signup.jsx
+++ frontend/src/signup/Signup.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./signup";
+import "./styles.css"
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Cookies from "js-cookie";
