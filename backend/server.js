fec2268105012dbbb06caa17bc3ca158ac5f45ec
Wesley Weisenberger <wesley@berkeley.edu>
2023-12-10 15:03:49 -0800

    add rate limiting to backend reqeusts

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,16 @@ mongoose.set("strictQuery", false);
 const bcrypt = require("bcrypt");
 const { v4: uuidv4 } = require("uuid");
 
+const { rateLimit } = require("express-rate-limit");
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter);
+
 const mongoDB = "mongodb://127.0.0.1:27017/todo-backend";
 var connectWithRetry = async function () {
   try {
